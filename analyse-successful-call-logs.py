ff1654134dbe2d8d10aa75399b403333a4e74416
Gorilliath <gorilliath59@gmail.com>
2024-04-05 01:07:21 +0100

    Renamed `Name` to `Caller`

--- analyse-successful-call-logs.py
+++ analyse-successful-call-logs.py
@@ -41,26 +41,26 @@ def parse_raw_text(raw_text):
     """
     Parses the constituent parts out of the raw text
 
-    Name, Duration (hours), Date Time
+    Caller, Duration (hours), Date Time
     e.g: Gorilliath started a call that lasted 2 hours. — 03/04/2024 23:06
     e.g: Gorilliath started a call. — 03/04/2024 23:06
     """
-    name_maybe_duration_part, date_time_part = raw_text.split(" — ")
+    caller_maybe_duration_part, date_time_part = raw_text.split(" — ")
 
-    # Name and Duration
-    if "that lasted" in name_maybe_duration_part:
-        name, duration_part = name_maybe_duration_part.split(
+    # Caller and Duration
+    if "that lasted" in caller_maybe_duration_part:
+        caller, duration_part = caller_maybe_duration_part.split(
             " started a call that lasted "
         )
         duration = normalise_duration_to_hours(duration_part)
     else:
-        name = name_maybe_duration_part.split(" started a call.")[0]
+        caller = caller_maybe_duration_part.split(" started a call.")[0]
         duration = 0
 
     # Date Time
     date_time = datetime.datetime.strptime(date_time_part, "%d/%m/%Y %H:%M")
 
-    return pd.Series([name, duration, date_time])
+    return pd.Series([caller, duration, date_time])
 
 
 if __name__ == "__main__":
@@ -68,7 +68,9 @@ if __name__ == "__main__":
     df = pd.read_csv("./output/successful-call-logs.csv")
 
     # Parse 'Raw Text' to it's constituent parts and extend the dataframe with it
-    df[["Name", "Duration (hours)", "Date Time"]] = df["Raw Text"].apply(parse_raw_text)
+    df[["Caller", "Duration (hours)", "Date Time"]] = df["Raw Text"].apply(
+        parse_raw_text
+    )
 
     # Write to a new CSV file
     df.to_csv("./output/analysed-successful-call-logs.csv", index=False)
@@ -80,7 +82,7 @@ if __name__ == "__main__":
             df,
             x="Date Time",
             y="Duration (hours)",
-            color="Name",
+            color="Caller",
             hover_data=["ID"],
             title="Duration of successful calls through time",
         )
